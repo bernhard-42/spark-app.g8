11a2d90dccf4ad03b21bcc9b848b6422d9d22585
Bernhard Walter <b_walter@arcor.de>
2021-05-20 15:06:48 +0200

    fixed path

---
+++ src/main/g8/src/main/scala/SparkApp.scala
@@ -0,0 +1,15 @@
+/* SparkApp.scala */
+package com.example
+import org.apache.spark.sql.SparkSession
+//import com.databricks.dbutils_v1.DBUtilsHolder.dbutils
+
+object SparkApp2 {
+  
+  def main(args: Array[String]) = {
+    val folder = args(0)
+    val version = args(1)
+
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val secret = com.databricks.dbutils_v1.DBUtilsHolder.dbutils.secrets.get("a_scope", "a_secret"))
+  }
+}
